2b036a783509d25515f69886eac06d187c93fa4e
RonakCODER19 <hedambharoank@gmail.com>
2024-07-04 18:37:22 -0400

    New File

--- C++/array.cpp
+++ C++/array.cpp
@@ -9,10 +9,11 @@ int main()
     {
         for(j=0; j<3; j++)
         {
-            cout<<"a" <<i<<j<<":";
+            cout<<"a"<<"="<<i<<j<<":";
             cin>>a[i][j];
 
         }
+        cout<<endl;
     }
 
 
@@ -20,7 +21,17 @@ int main()
     {
         for(j=0; j<3; j++)
         {
+            if(i==0 && j==0 || i==1 && j==0 || i==2 && j==0)
+            {
+                cout<<"|";
+            }
+            
             cout<<a[i][j];
+            
+             if(j==2 && i==0 || j==2 && i==1 || j==2 && i==2)
+            {
+                cout<<"|";
+            }
         }
         cout<<endl;
     }
