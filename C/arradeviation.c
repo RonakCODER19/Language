f171dfe45edbe630fde28f3c2ae358542b4e7dbb
RoankCODER19 <hedambharonak@gmail.com>
2024-07-20 17:20:30 -0400

    New file

---
+++ C/arradeviation.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void main()
+{
+    int row,col,a[2][2];
+        int r,c;
+
+        for(row=0; row<2; row++)
+        {
+            for(col=0; col<2; col++)
+            {
+                printf("a[%d][%d]:",row,col);
+                scanf("%d",&a[row][col]);
+            }
+            printf("\n");
+        }
+
+    printf("enter row to not print in array:");
+    scanf("%d",&r);
+    printf("enter col to not print array :");
+    scanf("%d",&c);
+
+        for(row=0; row<2; row++)
+        {
+            for(col=0; col<2; col++)
+            {
+                    if(row==r&&col==c)
+                    {
+                        printf("  ");
+                    }
+                    else
+                     printf("%d ",a[row][col]);
+            }
+            printf("\n");
+        }
+}
